938e08187a37c22961669e5f682802eb10dbbfde
mohit000001 <mohitofficial167@gmail.com>
2023-03-18 22:44:13 +0530

    Login text changed.

--- LoginPage.js
+++ LoginPage.js
@@ -1,12 +1,12 @@
-import { isFocusable } from '@testing-library/user-event/dist/utils';
-import React, { useState } from 'react';
+import { isFocusable } from "@testing-library/user-event/dist/utils";
+import React, { useState } from "react";
 
 function LoginPage(props) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
- /*
+  /*
   const handleUsernameChange = (event) => {
       setUsername(event.target.value);
   };
@@ -17,36 +17,45 @@ function LoginPage(props) {
 
   */
   const handleSubmit = (event) => {
-      event.preventDefault();
-      if (username === "" || password === "") {
-          setErrorMessage("Please enter a valid username and password.");
-      } else if (username !== "Ritu" || password !== "password") {
-          setErrorMessage("Invaid username or password.");
-      } else {
-          props.onLogin(username);
-      }
+    event.preventDefault();
+    if (username === "" || password === "") {
+      setErrorMessage("Please enter a valid username and password.");
+    } else if (username !== "Ritu" || password !== "password") {
+      setErrorMessage("Invaid username or password.");
+    } else {
+      props.onLogin(username);
+    }
   };
 
-
- 
   return (
-      <div className="login">
-          <h1>Login</h1>
-           {errorMessage && <p>{errorMessage}</p>}
-           <form onSubmit={handleSubmit}>
-               <div>
-                   <label>Username:</label>
-                   <input className="input" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-               </div>
-               <div>
-                   <label>Password:</label>
-                   <input className="input" type="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-               </div>
-                   <button className="loginbtn" type="submit">Login</button>
+    <div className="login">
+      <h1>Loginn</h1>
+      {errorMessage && <p>{errorMessage}</p>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input
+            className="input"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            className="input"
+            type="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button className="loginbtn" type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
 
-           </form>
-      </div>
-   );
-  }
-
-  export default LoginPage;
+export default LoginPage;
